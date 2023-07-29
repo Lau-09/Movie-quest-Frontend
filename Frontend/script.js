a32474032edf6fe4aa39c8212c81cbeafd5ae648
Lau-09 <118298572+Lau-09@users.noreply.github.com>
2023-07-28 21:16:15 -0500

    Add files via upload

---
+++ Frontend/script.js
@@ -0,0 +1,49 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const btnAbrir = document.getElementById("btnAbrir");
+    const btnCerrar = document.getElementById("btnCerrar");
+    const ventanaFlotante = document.getElementById("miVentanaFlotante");
+  
+    // Función para abrir la ventana flotante
+    btnAbrir.addEventListener("click", function () {
+      ventanaFlotante.style.display = "block";
+    });
+  
+    // Función para cerrar la ventana flotante
+    btnCerrar.addEventListener("click", function () {
+      ventanaFlotante.style.display = "none";
+    });
+  });
+
+document.addEventListener("DOMContentLoaded", function () {
+    const btnAbrir2 = document.getElementById("btnAbrir2");
+    const btnCerrar2 = document.getElementById("btnCerrar2");
+    const ventanaFlotante = document.getElementById("miVentanaFlotante2");
+  
+    // Función para abrir la ventana flotante
+    btnAbrir2.addEventListener("click", function () {
+      ventanaFlotante.style.display = "block";
+    });
+  
+    // Función para cerrar la ventana flotante
+    btnCerrar2.addEventListener("click", function () {
+      ventanaFlotante.style.display = "none";
+    });
+  });
+
+  /*
+document.addEventListener("DOMContentLoaded", function () {
+    const btnAbrir3 = document.getElementById("btnAbrir3");
+    const btnCerrar3 = document.getElementById("btnCerrar3");
+    const ventanaFlotante = document.getElementById("miVentanaFlotante3");
+  
+    // Función para abrir la ventana flotante
+    btnAbrir3.addEventListener("click", function () {
+      ventanaFlotante.style.display = "block";
+    });
+  
+    // Función para cerrar la ventana flotante
+    btnCerrar3.addEventListener("click", function () {
+      ventanaFlotante.style.display = "none";
+    });
+  });
+*/
